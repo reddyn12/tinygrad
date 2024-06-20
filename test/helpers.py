b4f11857e4a1ed4a7bc9a0bc18417784f1d0012f
reddyn12 <nikhilr.ssm@gmail.com>
2024-06-20 16:37:02 -0400

    Merge branch 'master' of https://github.com/tinygrad/tinygrad into retinanet_old

--- test/helpers.py
+++ test/helpers.py
@@ -1,6 +1,8 @@
-import sys
+import sys, unittest
 import numpy as np
 from tinygrad import Tensor, Device, dtypes
+from tinygrad.codegen.uops import UOp
+from tinygrad.tensor import _to_np_dtype
 from tinygrad.engine.realize import Runner
 from tinygrad.dtype import DType
 from tinygrad.nn.state import get_parameters
@@ -26,25 +28,33 @@ def assert_jit_cache_len(fxn, expected_len):
 def is_dtype_supported(dtype: DType, device: str = Device.DEFAULT):
   if dtype == dtypes.bfloat16:
     # NOTE: this requires bf16 buffer support
-    return device in {"HSA", "AMD"} or (device == "CUDA" and not CI and not getenv("PTX"))
+    return device in {"AMD"} or (device in {"CUDA", "NV"} and not CI and not getenv("PTX"))
   if device in ["WEBGPU", "WEBGL"]: return dtype in [dtypes.float, dtypes.int32, dtypes.uint32]
-  if device == "CUDA" and getenv("PTX") and dtype in (dtypes.int8, dtypes.uint8): return False
   # for CI GPU and OSX, cl_khr_fp16 isn't supported
   # for CI LLVM, it segfaults because it can't link to the casting function
   # CUDACPU architecture is sm_35 but we need at least sm_70 to run fp16 ALUs
   # PYTHON supports half memoryview in 3.12+ https://github.com/python/cpython/issues/90751
   if dtype == dtypes.half:
     if device == "GPU": return not CI and not OSX
-    if device in ["LLVM", "CUDA"]: return not CI
+    if device in ["LLVM", "CUDA", "NV"]: return not CI
     if device == "PYTHON": return sys.version_info >= (3, 12)
   if dtype == dtypes.float64: return device != "METAL" and not (OSX and device == "GPU")
   return True
 
 def rand_for_dtype(dt:DType, size:int):
   if dtypes.is_unsigned(dt):
-    return np.random.randint(0, 100, size=size, dtype=dt.np)
+    return np.random.randint(0, 100, size=size, dtype=_to_np_dtype(dt))
   elif dtypes.is_int(dt):
-    return np.random.randint(-100, 100, size=size, dtype=dt.np)
+    return np.random.randint(-100, 100, size=size, dtype=_to_np_dtype(dt))
   elif dt == dtypes.bool:
     return np.random.choice([True, False], size=size)
-  return np.random.uniform(-10, 10, size=size).astype(dt.np)
+  return np.random.uniform(-10, 10, size=size).astype(_to_np_dtype(dt))
+
+class TestUOps(unittest.TestCase):
+  def assert_equiv_uops(self, uop1:UOp, uop2:UOp):
+    # NOTE: direct UOps __eq__ is comparing object reference, use this function to compare two uops
+    self.assertIs(uop1.op, uop2.op)
+    self.assertEqual(uop1.dtype, uop2.dtype)
+    self.assertEqual(uop1.arg, uop2.arg)
+    self.assertEqual(len(uop1.src), len(uop2.src))
+    for s1, s2 in zip(uop1.src, uop2.src): self.assert_equiv_uops(s1, s2)
