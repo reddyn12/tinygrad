2b303b06b861fd8394306d23bb67f4b7769d1e64
reddyn12 <nikhilr.ssm@gmail.com>
2024-07-05 04:07:16 -0400

    120->160 it/s

--- examples/mlperf/helpers.py
+++ examples/mlperf/helpers.py
@@ -250,6 +250,7 @@ import os
 from collections import defaultdict
 import sys
 import mmap
+import pickle
 PYTHON_VERSION = sys.version_info[0]
 if PYTHON_VERSION == 2:
     from urllib import urlretrieve
@@ -263,57 +264,67 @@ def _isArrayLike(obj):
 
 class COCO:
     def __init__(self, annotation_file=None):
-        """
-        Constructor of Microsoft COCO helper class for reading and visualizing annotations.
-        :param annotation_file (str): location of annotation file
-        :param image_folder (str): location to the folder that hosts images.
-        :return:
-        """
-        # load dataset
-        self.dataset,self.anns,self.cats,self.imgs = dict(),dict(),dict(),dict()
-        self.imgToAnns, self.catToImgs = defaultdict(list), defaultdict(list)
-        if not annotation_file == None:
+        self.dataset = {}
+        self.anns = {}
+        self.cats = {}
+        self.imgs = {}
+        self.imgToAnns = defaultdict(list)
+        self.catToImgs = defaultdict(list)
+        
+        if annotation_file is not None:
             print('loading annotations into memory...')
             tic = time.time()
-            # dataset = json.load(open(annotation_file, 'r'))
-            with open(annotation_file, 'r+b') as f:
-                mm = mmap.mmap(f.fileno(), 0)
-                dataset = json.loads(mm.read().decode('utf-8'))
-            assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
-            print('Done (t={:0.2f}s)'.format(time.time()- tic))
-            self.dataset = dataset
-            self.createIndex()
+            
+            # Check if a pickled index file exists
+            index_file = annotation_file + '.pickle'
+            if os.path.exists(index_file):
+                with open(index_file, 'rb') as f:
+                    index_data = pickle.load(f)
+                self.anns = index_data['anns']
+                self.cats = index_data['cats']
+                self.imgs = index_data['imgs']
+                self.imgToAnns = index_data['imgToAnns']
+                self.catToImgs = index_data['catToImgs']
+                print(f'Loaded indexed data from {index_file}')
+            else:
+                # If no index file exists, create it
+                with open(annotation_file, 'r+b') as f:
+                    mm = mmap.mmap(f.fileno(), 0)
+                    self.dataset = json.loads(mm.read().decode('utf-8'))
+                assert isinstance(self.dataset, dict), 'annotation file format {} not supported'.format(type(self.dataset))
+                self.createIndex()
+                
+                # Save the index to a pickle file for future use
+                index_data = {
+                    'anns': self.anns,
+                    'cats': self.cats,
+                    'imgs': self.imgs,
+                    'imgToAnns': self.imgToAnns,
+                    'catToImgs': self.catToImgs
+                }
+                with open(index_file, 'wb') as f:
+                    pickle.dump(index_data, f)
+                print(f'Saved indexed data to {index_file}')
+            
+            print('Done (t={:0.2f}s)'.format(time.time() - tic))
 
     def createIndex(self):
-        # create index
         print('creating index...')
-        anns, cats, imgs = {}, {}, {}
-        imgToAnns,catToImgs = defaultdict(list),defaultdict(list)
-        if 'annotations' in self.dataset:
-            for ann in self.dataset['annotations']:
-                imgToAnns[ann['image_id']].append(ann)
-                anns[ann['id']] = ann
+        
+        # Create anns, imgs, and cats indices
+        for ann in self.dataset['annotations']:
+            self.imgToAnns[ann['image_id']].append(ann)
+            self.anns[ann['id']] = ann
+            if 'category_id' in ann:
+                self.catToImgs[ann['category_id']].append(ann['image_id'])
 
-        if 'images' in self.dataset:
-            for img in self.dataset['images']:
-                imgs[img['id']] = img
+        for img in self.dataset['images']:
+            self.imgs[img['id']] = img
 
-        if 'categories' in self.dataset:
-            for cat in self.dataset['categories']:
-                cats[cat['id']] = cat
-
-        if 'annotations' in self.dataset and 'categories' in self.dataset:
-            for ann in self.dataset['annotations']:
-                catToImgs[ann['category_id']].append(ann['image_id'])
+        for cat in self.dataset['categories']:
+            self.cats[cat['id']] = cat
 
         print('index created!')
-
-        # create class members
-        self.anns = anns
-        self.imgToAnns = imgToAnns
-        self.catToImgs = catToImgs
-        self.imgs = imgs
-        self.cats = cats
 
     def info(self):
         """
@@ -324,108 +335,60 @@ class COCO:
             print('{}: {}'.format(key, value))
 
     def getAnnIds(self, imgIds=[], catIds=[], areaRng=[], iscrowd=None):
-        """
-        Get ann ids that satisfy given filter conditions. default skips that filter
-        :param imgIds  (int array)     : get anns for given imgs
-               catIds  (int array)     : get anns for given cats
-               areaRng (float array)   : get anns for given area range (e.g. [0 inf])
-               iscrowd (boolean)       : get anns for given crowd label (False or True)
-        :return: ids (int array)       : integer array of ann ids
-        """
-        imgIds = imgIds if _isArrayLike(imgIds) else [imgIds]
-        catIds = catIds if _isArrayLike(catIds) else [catIds]
+        imgIds = set(imgIds if isinstance(imgIds, list) else [imgIds])
+        catIds = set(catIds if isinstance(catIds, list) else [catIds])
 
         if len(imgIds) == len(catIds) == len(areaRng) == 0:
-            anns = self.dataset['annotations']
+            anns = self.anns.values()
         else:
-            if not len(imgIds) == 0:
-                lists = [self.imgToAnns[imgId] for imgId in imgIds if imgId in self.imgToAnns]
-                anns = list(itertools.chain.from_iterable(lists))
+            if not imgIds:
+                anns = self.anns.values()
             else:
-                anns = self.dataset['annotations']
-            anns = anns if len(catIds)  == 0 else [ann for ann in anns if ann['category_id'] in catIds]
-            anns = anns if len(areaRng) == 0 else [ann for ann in anns if ann['area'] > areaRng[0] and ann['area'] < areaRng[1]]
-        if not iscrowd == None:
-            ids = [ann['id'] for ann in anns if ann['iscrowd'] == iscrowd]
-        else:
-            ids = [ann['id'] for ann in anns]
-        return ids
+                anns = [ann for imgId in imgIds for ann in self.imgToAnns[imgId]]
+            anns = [ann for ann in anns if not catIds or ann['category_id'] in catIds]
+            if areaRng:
+                anns = [ann for ann in anns if areaRng[0] < ann['area'] < areaRng[1]]
+        if iscrowd is not None:
+            anns = [ann for ann in anns if ann['iscrowd'] == iscrowd]
+        return [ann['id'] for ann in anns]
 
     def getCatIds(self, catNms=[], supNms=[], catIds=[]):
-        """
-        filtering parameters. default skips that filter.
-        :param catNms (str array)  : get cats for given cat names
-        :param supNms (str array)  : get cats for given supercategory names
-        :param catIds (int array)  : get cats for given cat ids
-        :return: ids (int array)   : integer array of cat ids
-        """
-        catNms = catNms if _isArrayLike(catNms) else [catNms]
-        supNms = supNms if _isArrayLike(supNms) else [supNms]
-        catIds = catIds if _isArrayLike(catIds) else [catIds]
+        catNms = set(catNms if isinstance(catNms, list) else [catNms])
+        supNms = set(supNms if isinstance(supNms, list) else [supNms])
+        catIds = set(catIds if isinstance(catIds, list) else [catIds])
 
-        if len(catNms) == len(supNms) == len(catIds) == 0:
-            cats = self.dataset['categories']
-        else:
-            cats = self.dataset['categories']
-            cats = cats if len(catNms) == 0 else [cat for cat in cats if cat['name']          in catNms]
-            cats = cats if len(supNms) == 0 else [cat for cat in cats if cat['supercategory'] in supNms]
-            cats = cats if len(catIds) == 0 else [cat for cat in cats if cat['id']            in catIds]
-        ids = [cat['id'] for cat in cats]
-        return ids
+        if not catNms and not supNms and not catIds:
+            return list(self.cats.keys())
+        cats = self.cats.values()
+        if catNms:
+            cats = [cat for cat in cats if cat['name'] in catNms]
+        if supNms:
+            cats = [cat for cat in cats if cat['supercategory'] in supNms]
+        if catIds:
+            cats = [cat for cat in cats if cat['id'] in catIds]
+        return [cat['id'] for cat in cats]
 
     def getImgIds(self, imgIds=[], catIds=[]):
-        '''
-        Get img ids that satisfy given filter conditions.
-        :param imgIds (int array) : get imgs for given ids
-        :param catIds (int array) : get imgs with all given cats
-        :return: ids (int array)  : integer array of img ids
-        '''
-        imgIds = imgIds if _isArrayLike(imgIds) else [imgIds]
-        catIds = catIds if _isArrayLike(catIds) else [catIds]
+        imgIds = set(imgIds if isinstance(imgIds, list) else [imgIds])
+        catIds = set(catIds if isinstance(catIds, list) else [catIds])
 
-        if len(imgIds) == len(catIds) == 0:
-            ids = self.imgs.keys()
-        else:
-            ids = set(imgIds)
-            for i, catId in enumerate(catIds):
-                if i == 0 and len(ids) == 0:
-                    ids = set(self.catToImgs[catId])
-                else:
-                    ids &= set(self.catToImgs[catId])
-        return list(ids)
+        if not imgIds and not catIds:
+            return list(self.imgs.keys())
+        if not catIds:
+            return list(imgIds & set(self.imgs.keys()))
+        imgIds = set.union(*[set(self.catToImgs[catId]) for catId in catIds]) if not imgIds else imgIds
+        return list(imgIds & set(self.imgs.keys()))
 
     def loadAnns(self, ids=[]):
-        """
-        Load anns with the specified ids.
-        :param ids (int array)       : integer ids specifying anns
-        :return: anns (object array) : loaded ann objects
-        """
-        if _isArrayLike(ids):
-            return [self.anns[id] for id in ids]
-        elif type(ids) == int:
-            return [self.anns[ids]]
+        return [self.anns[id] for id in ids] if isinstance(ids, list) else [self.anns[ids]]
+
 
     def loadCats(self, ids=[]):
-        """
-        Load cats with the specified ids.
-        :param ids (int array)       : integer ids specifying cats
-        :return: cats (object array) : loaded cat objects
-        """
-        if _isArrayLike(ids):
-            return [self.cats[id] for id in ids]
-        elif type(ids) == int:
-            return [self.cats[ids]]
+        return [self.cats[id] for id in ids] if isinstance(ids, list) else [self.cats[ids]]
 
     def loadImgs(self, ids=[]):
-        """
-        Load anns with the specified ids.
-        :param ids (int array)       : integer ids specifying img
-        :return: imgs (object array) : loaded img objects
-        """
-        if _isArrayLike(ids):
-            return [self.imgs[id] for id in ids]
-        elif type(ids) == int:
-            return [self.imgs[ids]]
+        return [self.imgs[id] for id in ids] if isinstance(ids, list) else [self.imgs[ids]]
+
 
     def loadRes(self, resFile):
         """
